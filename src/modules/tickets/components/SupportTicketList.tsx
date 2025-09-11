eb090b47b1872de170bab9b5fbdddf168a5d120d
Gustavo Macedo <mota.macedo05@gmail.com>
2025-09-11 13:25:19 -0300

    atualiza tamanho do card na lista

--- src/modules/tickets/components/SupportTicketList.tsx
+++ src/modules/tickets/components/SupportTicketList.tsx
@@ -28,7 +28,7 @@ export default function SupportTicketsList() {
   if (tickets.length === 0) return <p className="text-center py-10">Nenhum ticket encontrado.</p>;
 
   return (
-    <div className="max-w-4xl mx-auto p-4">
+    <div className="max-w-full mx-auto p-4">
       {tickets.map((ticket) => (
         <TicketCard key={ticket.id} ticket={ticket} />
       ))}
